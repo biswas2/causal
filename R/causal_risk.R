0f0ac443d1c7a8b00c522dbd4dae04e558a97cd9
Rasel Biswas <rbiswas1@isrt.ac.bd>
2020-02-11 16:53:34 +0600

    Update causal_risk.R

--- R/causal_risk.R
+++ R/causal_risk.R
@@ -1,12 +1,14 @@
-#' Calculate measures of causal effects for conditionally randomized experiments
+#' @title Calculate measures of causal effects for conditionally randomized experiments
 #'
-#' This function computes the causal risk measures in a conditionally randomized experiment via standardization.
-
+#' @description This function computes the causal risk measures in a conditionally randomized experiment via standardization.
+#'
 #' @param L Vector of the values of L, on which the experiment is conditionally randomized.
 #' @param A Vector of treatment assignment. A = 1, if the individual received the treatment; and A = 0, if the individual did not.
 #' @param Y Vector of outcome values.  Y = 1, if the individual died; and A = 0, otherwise.
 #' @param data (Optional) The data frame containing the A, L, and Y variables by their exact names. If this argument is provided, then the A, L, and Y arguments become optional.
-#' @return Measures of causal effects (e.g., risk difference, risk ratio, and odds ratio).
+#'
+#' @return Measures of causal effects (e.g., risk difference, risk ratio, odds ratio).
+#'
 #' @examples
 #' L <- rep(x = c(0,1), times = c(7, 13))
 #' A <- c(0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1)
@@ -32,17 +34,14 @@ causal_risk <- function(A, L, Y, data = NULL) {
     prop.table(table(dat$L)) %*% as.matrix(prob$m[prob$A == 1])
   risk_in_non_treated <-
     prop.table(table(dat$L)) %*% as.matrix(prob$m[prob$A == 0])
+
   results <- data.frame(
     row.names = c(
-      "Counterfactual risk in the treated",
-      "Counterfactual risk in the non-treated",
       "Causal risk difference",
       "Causal risk ratio",
       "Causal odds ratio"
     ),
     values = c(
-      risk_in_treated,
-      risk_in_non_treated,
       as.numeric(risk_in_treated - risk_in_non_treated),
       as.numeric(risk_in_treated / risk_in_non_treated),
       as.numeric((risk_in_treated / (1 - risk_in_treated)) /
